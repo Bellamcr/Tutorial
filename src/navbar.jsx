3343c8d4a12ca7afafc5efd05aabe46c5cfd0f92
Izabella Ribeiro <112509117+Bellamcr@users.noreply.github.com>
2023-11-03 19:51:34 -0600

    Initial commit

---
+++ src/navbar.jsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/kitchen/">KITCHEN</a>
+        </li>
+        <li>
+          <a href="/living-room">LIVING ROOM</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
